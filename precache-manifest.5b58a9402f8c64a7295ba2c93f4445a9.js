dbcb5790ac0167f53573ba62ba61960b823e0296
rezamini <reza.amini.b@gmail.com>
2020-02-19 03:44:03 +0100

    Updates

---
+++ precache-manifest.5b58a9402f8c64a7295ba2c93f4445a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a34ca910167799d980be8ab99569269",
+    "url": "/react-simple-dashboard/index.html"
+  },
+  {
+    "revision": "230258d033ae45f424f2",
+    "url": "/react-simple-dashboard/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c51fa6262b4b8365fe69",
+    "url": "/react-simple-dashboard/static/css/main.50a787c3.chunk.css"
+  },
+  {
+    "revision": "230258d033ae45f424f2",
+    "url": "/react-simple-dashboard/static/js/2.fd7efd82.chunk.js"
+  },
+  {
+    "revision": "a9cf26190318d62f46aa96a20e15dd9f",
+    "url": "/react-simple-dashboard/static/js/2.fd7efd82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51fa6262b4b8365fe69",
+    "url": "/react-simple-dashboard/static/js/main.f1babf20.chunk.js"
+  },
+  {
+    "revision": "57074e9f6d28e5c29791",
+    "url": "/react-simple-dashboard/static/js/runtime-main.db473422.js"
+  },
+  {
+    "revision": "89a73eb814faa0fb7a55c54257fff371",
+    "url": "/react-simple-dashboard/static/media/dashboard.89a73eb8.png"
+  }
+]);
